03dd2586350643c562e40ca15cc1eca05135b07c
Mateo Cortes <tomic.ero1@gmail.com>
2023-03-04 13:37:34 -0500

    Actividad1_terminada.v2

--- Ejercicio_5/src/ejercicio5/Ejercicio5.java
+++ Ejercicio_5/src/ejercicio5/Ejercicio5.java
@@ -1,16 +1,22 @@
 package ejercicio5;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
 		
+		Scanner entrada = new Scanner(System.in);
+		
 		double suma = 0;		// se inicializa la variable suma de tipo doble.
 		
-		double X = 20;			// se inicializa la variable X de tipo doble.
+		System.out.println("Ingrese el valor de X: ");
+		double X = entrada.nextDouble();			// se inicializa la variable X de tipo doble.
+		
+		System.out.println("Ingrese el valor de Y: ");
+		double Y = entrada.nextDouble();			// se inicializa la variable Y de tipo doble.
 		
 		suma += X;				// se incrementa suma X veces.
-		
-		int Y = 40;				// se inicializa la variable Y de tipo doble.
 		
 		/* Las variables anteriores se eligieron de tipo doble para usar el método
 		 * pow de la clase Math e imprimir al final el valor de suma con decimales
@@ -22,6 +28,8 @@ public class Ejercicio5 {
 		suma += (double)X/Y;
 		
 		System.out.println("La suma es: " + suma);
+		
+		entrada.close();
 	}
 
 }
